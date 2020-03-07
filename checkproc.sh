2e0d013161880052ce674443ebd45d29fd4987b6
wocanmei <wocanmei@qq.com>
2020-03-07 23:57:01 +0800

    check proc is runing or not
    check proc

---
+++ checkproc.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PROC_NAME="Process Name"
+ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`
+if [ $ProcNumber -le 0 ];then
+   ## CMD
+   ;;   
+else
+   ## PASS
+   ;;
+fi
